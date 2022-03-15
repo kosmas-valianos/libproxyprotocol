cf4059c79e11de5d7d5495804d918e5063aecd4b
kosmas.valianos <kosmas.valianos@appgate.com>
2022-03-15 23:55:05 +0100

    GNU Lesser General Public License v3.0

--- src/proxy_protocol.h
+++ src/proxy_protocol.h
@@ -1,3 +1,21 @@
+/*
+ * c-proxy-protocol is an ANSI C library to parse and create PROXY protocol v1 and v2 headers
+ * Copyright (C) 2022  Kosmas Valianos (kosmas.valianos@gmail.com)
+ *
+ * The c-proxy-protocol library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The c-proxy-protocol library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef PROXY_PROTOCOL_H
 #define PROXY_PROTOCOL_H
 
