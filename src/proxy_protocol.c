dcbe6ae03e81c9a77a209a4ef727f767d5e6b1ce
Kosmas Valianos <kosmas.valianos@gmail.com>
2026-03-16 14:27:53 +0100

    Simplify PP2_SUBTYPE_AZURE_PRIVATEENDPOINT_LINKID parsing

--- src/proxy_protocol.c
+++ src/proxy_protocol.c
@@ -1213,8 +1213,7 @@ static int32_t pp2_parse_hdr(const uint8_t *buffer, uint32_t buffer_length, pp_i
             /* Connection is done through Private Link service */
             if (pp2_tlv_azure->type == PP2_SUBTYPE_AZURE_PRIVATEENDPOINT_LINKID) /* 32-bit number */
             {
-                pp2_tlv_t *tlv = tlv_new(pp2_tlv->type, pp2_tlv_len, pp2_tlv->value);
-                if (!tlv || !tlv_array_append_tlv(&pp_info->pp2_info.tlv_array, tlv))
+                if (!tlv_array_append_tlv_new(&pp_info->pp2_info.tlv_array, pp2_tlv->type, pp2_tlv_len, pp2_tlv->value))
                 {
                     return -ERR_HEAP_ALLOC;
                 }
